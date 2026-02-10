7171f530dc71fd077d6ea9e3d7fc1fa5bde100ef
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2026-02-10 23:11:06 +0300

    Main Page remeik

--- main.tsx
+++ main.tsx
@@ -1,10 +1,11 @@
-import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './src/index.css'
 import App from './src/app/App.tsx'
 import {store} from "./src/app/store";
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
+
+
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
