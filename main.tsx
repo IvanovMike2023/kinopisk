d1b8e1f9a75193d8e2b9f2164b3db5ebe534c09e
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2026-02-12 23:11:07 +0300

    Main Page remeik3

--- main.tsx
+++ main.tsx
@@ -5,8 +5,6 @@ import {store} from "./src/app/store";
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
 
-
-
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <Provider store={store}>
