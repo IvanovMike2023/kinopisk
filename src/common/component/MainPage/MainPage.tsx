9d0c9b0606d74059397b34f9fda13bdcb3c9ed48
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2026-03-02 23:32:45 +0300

    const [activeCategory, setActiveCategory] = useState('Popular')

--- src/common/component/MainPage/MainPage.tsx
+++ src/common/component/MainPage/MainPage.tsx
@@ -6,29 +6,29 @@ import {PopularMovies} from "../PopularMovies/PopularMovies";
 
 export const MainPage = () => {
     const [backdrop_path, setBackdrop_path] = useState('');
-    const {data} = useGetPopularQuery({page:1})
+    const {data} = useGetPopularQuery({page: 1})
 
     useEffect(() => {
         const backdrop_path_number = Math.floor(Math.random() * data?.results.length)
         const url = data?.results[backdrop_path_number].backdrop_path
         setBackdrop_path(url)
     }, [data])
-     const popular_movies =data?.results? data?.results.slice().sort((a, b) => b.vote_average - a.vote_average).slice(0, 6):[]
+    const popular_movies = data?.results ? data?.results.slice().sort((a, b) => b.vote_average - a.vote_average).slice(0, 6) : []
     return <div className={s.Container}>
-    <section className={s.page} >
-    <section style={{backgroundImage: `url(https://image.tmdb.org/t/p/original/${backdrop_path})`}}
-                    className={s.section}>
-        <div className={s.content}>
-            <h1 className={s.title}>Welcome</h1>
-            <h2 className={s.subtitle}>Browse highlighted titles from TMDB</h2>
-            <SearchInput />
-        </div>
+        <section className={s.page}>
+            <section style={{backgroundImage: `url(https://image.tmdb.org/t/p/original/${backdrop_path})`}}
+                     className={s.section}>
+                <div className={s.content}>
+                    <h1 className={s.title}>Welcome</h1>
+                    <h2 className={s.subtitle}>Browse highlighted titles from TMDB</h2>
+                    <SearchInput/>
+                </div>
 
-    </section>
-        <div className={s.popularMovies} >
-                <PopularMovies popular_movies={popular_movies}  />
+            </section>
+            <div className={s.popularMovies}>
+                <PopularMovies popular_movies={popular_movies}/>
 
-        </div>
-    </section>
+            </div>
+        </section>
     </div>
 }
