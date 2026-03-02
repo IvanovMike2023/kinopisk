5481e6592bd2ce755b14c393343b70572027fd60
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2026-03-02 23:19:49 +0300

    CategoryMovies Button   setPage(1)

--- src/common/component/CategoryMoviesPage/CategoryMovies.tsx
+++ src/common/component/CategoryMoviesPage/CategoryMovies.tsx
@@ -1,46 +1,94 @@
-import {useGetPopularQuery} from "../MainPage/api/mainPageApi";
+import {
+    useGetNowPlayingQuery,
+    useGetPopularQuery,
+    useGetTopRatedQuery,
+    useGetUpcomingQuery
+} from "../MainPage/api/mainPageApi";
 import {SearchResult} from "../SearchPage/SearchResult/SearchResult";
 import {Pagination} from "../../Pagination/Pagination";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Button, useTheme} from "@mui/material";
 import s from "./CategoryMovies.module.css";
 
-export const CategoryMovies=()=>{
+export const CategoryMovies = () => {
     const [page, setPage] = useState(1);
+    const [dataitem, setDataitem] = useState('Popular');
 
-    const {data,refetch} = useGetPopularQuery({page})
-    const currentPage = data?.page
-    const count = data?.total_pages
+    const {data: Popular, refetch: refetchPopular} = useGetPopularQuery({page})
+    const {data: topRatedData, refetch: refetchTopRated} = useGetTopRatedQuery({page});
+    const {data: UpcomingData, refetch: refetchUpcoming} = useGetUpcomingQuery({page});
+    const {data: NowPlayingData, refetch: refetchNowPlaying} = useGetNowPlayingQuery({page});
+    const [results, setResults] = useState(Popular);
+
+    const currentPage = results?.page
+    const count = results?.total_pages
     const theme = useTheme();
     const setCurrentPage = (newpage) => {
-        console.log(newpage)
         setPage(newpage)
-        refetch({page:newpage})
     }
+    const getPopularMovies = () => {
+        refetchPopular().unwrap().then(() => {
+            setResults(Popular || [])
+            setDataitem('Popular')
+            setPage(1)
+        });
+    }
+    const getTopRated = () => {
+        refetchTopRated().unwrap().then(() => {
+            setResults(topRatedData || [])
+            setDataitem('TopRated')
+            setPage(1)
+        });
+    }
+    const Upcoming = () => {
+        refetchUpcoming().unwrap().then(() => {
+            setResults(UpcomingData || [])
+            setDataitem('Upcoming')
+            setPage(1)
+        });
+    }
+    const NowPlaying = () => {
+        refetchNowPlaying().unwrap().then(() => {
+            setResults(NowPlayingData || [])
+            setDataitem('NowPlaying')
+            setPage(1)
+        });
+    }
+    useEffect(() => {
+        if (dataitem === 'Popular') {
+            setResults(Popular || []);
+        } else if (dataitem === 'TopRated') {
+            setResults(topRatedData || []);
+        } else if (dataitem === 'Upcoming') {
+            setResults(UpcomingData || []);
+        } else if (dataitem === 'NowPlaying') {
+            setResults(NowPlayingData || []);
+        }
+    }, [Popular, topRatedData, UpcomingData, NowPlayingData, dataitem]);
     return <div className={s.container}>
         <section className={s.section}>
-        <div className={s.category} >
-            <div className={s.categoryButtons}>
-            <Button variant="contained">Contained</Button>
-            <Button variant="contained">Contained</Button>
-            <Button variant="contained">Contained</Button>
-            <Button variant="contained">Contained</Button>
-        </div>
-        </div>
-        <h2 style={{ color: theme.palette.text.primary }} className={s.titleResult}>Popular Movies</h2>
-        <div className={s.movies}>
-            {data?.results.map((el) => (
-                <SearchResult
-                    key={el.id}
-                    vote_average={el.vote_average}
-                    title={el.title}
-                    poster_path={el.poster_path}
-                />
-            ))}
-        </div>
-        <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage}
-                    count={count}/>
+            <div className={s.category}>
+                <div className={s.categoryButtons}>
+                    <Button onClick={getPopularMovies} variant="contained">Popular Movies</Button>
+                    <Button onClick={getTopRated} variant="contained">Top Rated Movies</Button>
+                    <Button onClick={Upcoming} variant="contained">Upcoming Movies</Button>
+                    <Button onClick={NowPlaying} variant="contained">Now Playing Movies</Button>
+                </div>
+            </div>
+            <h2 style={{color: theme.palette.text.primary}} className={s.titleResult}>Popular Movies</h2>
+            <div className={s.movies}>
+                {results?.results.map((el) => (
+                    <SearchResult
+                        key={el.id}
+                        vote_average={el.vote_average}
+                        title={el.title}
+                        poster_path={el.poster_path}
+                    />
+                ))}
+            </div>
+            <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage}
+                        count={count}/>
 
-    </section>
+        </section>
     </div>
 }
--- src/common/component/MainPage/api/mainPageApi.ts
+++ src/common/component/MainPage/api/mainPageApi.ts
@@ -7,8 +7,23 @@ export const mainPageApi = baseApi.injectEndpoints({
             query: ({page}) => ({url: `/movie/popular?language=en-US&page=${page}`}) ,
             providesTags: ['Popular']
         }),
+        getTopRated: build.query<PopularType, void>({
+            query: ({page}) => ({url: `movie/top_rated?page=${page}`}),
+            providesTags: ['Popular']
+        }),
+        getUpcoming: build.query<PopularType, void>({
+            query: ({page}) => ({url:`movie/upcoming?page=${page}`}),
+            providesTags: ['Popular']
+        }),
+        getNowPlaying: build.query<PopularType, void>({
+            query: ({page}) => ({url:`movie/now_playing?page=${page}`}),
+            providesTags: ['Popular']
+        }),
     }),
 })
 export const {
     useGetPopularQuery,
+    useGetTopRatedQuery,
+    useGetUpcomingQuery,
+    useGetNowPlayingQuery,
 } = mainPageApi
