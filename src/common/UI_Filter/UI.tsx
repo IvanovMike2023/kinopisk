0a735bd866b778fb2f083c6824f8ab23f02316a8
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2026-03-05 18:38:38 +0300

    FilteredMovies buttons qwes

--- src/common/UI_Filter/UI.tsx
+++ src/common/UI_Filter/UI.tsx
@@ -4,10 +4,10 @@ import FormControl from "@mui/material/FormControl";
 import Box from "@mui/material/Box";
 import MenuItem from "@mui/material/MenuItem";
 
-type Props={
-    name:string
+type Props = {
+    name: string
 }
-export const UI=({name}):Props=>{
+export const UI = ({name}): Props => {
     const theme = useTheme();
 
     return (<Button
@@ -25,26 +25,26 @@ export const UI=({name}):Props=>{
     >{name}
     </Button>)
 }
-type Sort={
-    value:string,
-    handleChange:()=>void
+type Sort = {
+    value: string,
+    handleChange: () => void
 }
-export const SortingSelector = ({ value, handleChange }):Sort => {
-const theme = useTheme();
-  return  <FormControl sx={{ width: 200 }}>
-        <Select       value={value}
-                      defaultValue="someValue"
-                      onChange={handleChange}
-                      MenuProps={{
-                          PaperProps: {
-                              style: { backgroundColor: theme.palette.background.default }, // цвет фона меню
-                          },
-                      }}
-        sx={{
-            height:35
-        }}
+export const SortingSelector = ({value, handleChange}): Sort => {
+    const theme = useTheme();
+    return <FormControl sx={{width: 200}}>
+        <Select value={value}
+                defaultValue="someValue"
+                onChange={handleChange}
+                MenuProps={{
+                    PaperProps: {
+                        style: {backgroundColor: theme.palette.background.default}, // цвет фона меню
+                    },
+                }}
+                sx={{
+                    height: 35
+                }}
         >
-            <MenuItem  value={'popularity.desc'}>Popularity ↓</MenuItem>
+            <MenuItem value={'popularity.desc'}>Popularity ↓</MenuItem>
             <MenuItem value={'popularity.asc'}>Popularity ↑</MenuItem>
             <MenuItem value={'Rating ↓'}>Rating ↓</MenuItem>
             <MenuItem value={'Rating ↑'}>Rating ↑</MenuItem>
@@ -53,51 +53,51 @@ const theme = useTheme();
             <MenuItem value={'Title A-Z'}>Title A-Z</MenuItem>
             <MenuItem value={'Thirty'}>Thirty</MenuItem>
             <MenuItem value={'Title Z-A'}>Title Z-A</MenuItem>
-    </Select>
-</FormControl>
+        </Select>
+    </FormControl>
 }
-type Rating={
-    range:number,
-    onChangeSlider:()=>void
+type Rating = {
+    range: number,
+    onChangeSlider: () => void
 }
-export  const RatingRangeSlider = ({ range, onChangeSlider }):Rating => {
+export const RatingRangeSlider = ({range, onChangeSlider}): Rating => {
     const theme = useTheme();
-    return  <Box sx={{ width: '100%', maxWidth: 250, padding: 2, borderRadius: 2 }}>
+    return <Box sx={{width: '100%', maxWidth: 250, padding: 2, borderRadius: 2}}>
         <Box display="flex" justifyContent="space-between" alignItems="center" mb={2}>
-    <Typography variant="subtitle1" fontWeight="bold">
-        Rating Range
-    </Typography>
-    <Typography variant="subtitle1" sx={{ color: theme.palette.text.primary }}>
-        {range[0].toFixed(1)} - {range[1].toFixed(1)}
-    </Typography>
-</Box>
-    {/* Двойной слайдер */}
-    <Slider
-        value={range}
-        onChange={onChangeSlider}
-        min={0.0}
-        max={10.0}
-        step={0.1}
-        valueLabelDisplay="off"
-        disableSwap //
-        sx={{
-            width: '100%',
-            color: 'blue', // цвет слайдера
-            '& .MuiSlider-thumb': {
-                backgroundColor: 'blue',
-                border: '2px solid blue',
-            },
-            '& .MuiSlider-track': {
-                height: 4,
-                backgroundColor: 'blue', // цвет полосы
-            },
-            '& .MuiSlider-rail': {
-                height: 4,
-                backgroundColor: '#ccc',
-            },
-        }}
-    />
+            <Typography variant="subtitle1" fontWeight="bold">
+                Rating Range
+            </Typography>
+            <Typography variant="subtitle1" sx={{color: theme.palette.text.primary}}>
+                {range[0].toFixed(1)} - {range[1].toFixed(1)}
+            </Typography>
+        </Box>
+        {/* Двойной слайдер */}
+        <Slider
+            value={range}
+            onChange={onChangeSlider}
+            min={0.0}
+            max={10.0}
+            step={0.1}
+            valueLabelDisplay="off"
+            disableSwap //
+            sx={{
+                width: '100%',
+                color: 'blue', // цвет слайдера
+                '& .MuiSlider-thumb': {
+                    backgroundColor: 'blue',
+                    border: '2px solid blue',
+                },
+                '& .MuiSlider-track': {
+                    height: 4,
+                    backgroundColor: 'blue', // цвет полосы
+                },
+                '& .MuiSlider-rail': {
+                    height: 4,
+                    backgroundColor: '#ccc',
+                },
+            }}
+        />
     </Box>
-    }
+}
 
 
