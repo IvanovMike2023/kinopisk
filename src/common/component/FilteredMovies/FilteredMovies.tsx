a2109550c807a57b8f910b5268d37d86fe4092a1
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2026-03-10 22:33:34 +0300

    FilteredMovies raiting

--- src/common/component/FilteredMovies/FilteredMovies.tsx
+++ src/common/component/FilteredMovies/FilteredMovies.tsx
@@ -5,27 +5,27 @@ import {Filters_Sort} from "./Filters_Sort/Filters_Sort";
 import {useState} from "react";
 
 export const FilteredMovies = () => {
-    const [params, setParams] = useState({page: 20, sort_by: 'original_title.asc'});
+    const [params, setParams] = useState({page: 1, sort_by: 'original_title.asc'});
     const {data, refetch} = useGetDiscoverMovieQuery({payload: params})
     const selectFilter = (value) => {
         switch (value) {
             case 'popularity.asc' :
-                setParams({page: 20, sort_by: 'popularity.asc'})
+                setParams({page: 1, sort_by: 'popularity.asc'})
                 break
             case 'popularity.desc':
-                setParams({page: 20, sort_by: 'popularity.desc'})
+                setParams({page: 1, sort_by: 'popularity.desc'})
                 break
             case 'vote_average.asc':
-                setParams({page: 20, sort_by: 'vote_average.asc'})
+                setParams({page: 1, sort_by: 'vote_average.asc'})
                 break
             case 'vote_average.desc':
-                setParams({page: 20, sort_by: 'vote_average.desc'})
+                setParams({page: 1, sort_by: 'vote_average.desc'})
                 break
             case 'primary_release_date.desc':
-                setParams({page: 20, sort_by: 'primary_release_date.desc'})
+                setParams({page: 1, sort_by: 'primary_release_date.desc'})
                 break
             case 'primary_release_date.asc':
-                setParams({page: 20, sort_by: 'primary_release_date.asc'})
+                setParams({page: 1, sort_by: 'primary_release_date.asc'})
                 break
             case 'original_title.asc':
                 setParams({page: 20, sort_by: 'original_title.asc'})
