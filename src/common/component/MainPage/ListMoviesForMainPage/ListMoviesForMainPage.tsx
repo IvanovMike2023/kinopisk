c1185727ec0b7bc4f419132ddb46283a91cc8cde
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2026-03-17 12:10:33 +0300

    почему нельзя мутировать state (push вместо spread)

--- src/common/component/MainPage/ListMoviesForMainPage/ListMoviesForMainPage.tsx
+++ src/common/component/MainPage/ListMoviesForMainPage/ListMoviesForMainPage.tsx
@@ -5,6 +5,7 @@ import {useNavigate} from "react-router-dom";
 import {Button, useTheme} from "@mui/material";
 import {addFilmsToStorage} from '../../../helper/addFilmsToStorage'
 import {useState} from "react";
+
 type Props = {
     data: ResultsPopular[]
     title: string
@@ -32,25 +33,35 @@ export const ListMoviesForMainPage = ({data, title}: Props) => {
         }
 
     }
-    const [isLike,setIsLike]=useState(true)
-const handleLike=(e)=>{
-        !isLike?
-        setIsLike(true) : setIsLike(false)
+    const [likedId, setLikedId] = useState<number[]>([])
+    const handleLike = (e) => {
+        e.preventDefault()
+        const idLike = Number(e.currentTarget.id)
 
-    e.preventDefault()
-    const idLike=Number(e.currentTarget.id)
+        setLikedId((prev) => {
+            if (prev.includes(idLike)) {
+                const update = prev.filter((f) => f !== idLike)
+                const stored = localStorage.getItem('films')
+                const films = stored ? JSON.parse(stored) : []
+                const newfilms = films.filter((f) => f.id != idLike)
+                localStorage.setItem('films', JSON.stringify(newfilms))
+                return update
+            } else {
+                const result = data.find((el) => el.id === idLike)
+                if (result) {
+                    const newFilm = {
+                        id: result.id,
+                        title: result.title,
+                        backdrop_path: result.backdrop_path,
+                        vote_average: result.vote_average
+                    };
+                    addFilmsToStorage(newFilm)
+                }
+                return [...prev, idLike]
+            }
+        })
 
-    if(!isLike){
-        const stored = localStorage.getItem('films')
-        const films = stored ? JSON.parse(stored) : []
-        const newfilms= films.filter((f)=>f.id!=idLike)
-        localStorage.setItem('films', JSON.stringify(newfilms))
     }
-
-    const result=data.find((f)=>f.id===idLike)
-    const newFilm={id:result.id,title: result.title,backdrop_path: result.backdrop_path,vote_average: result.vote_average};
-    addFilmsToStorage(newFilm)
-}
     return <section className={s.section_popular}>
         <div className={s.headerPopular}>
             <h2>{title}</h2>
@@ -63,9 +74,9 @@ const handleLike=(e)=>{
                         <a className={s.posterLink}
                            href="src/common/component/MainPage/ListMoviesForMainPage/ListMoviesForMainPage">
                             <img className={s.poster} src={'https://image.tmdb.org/t/p/w185' + el.poster_path} alt=""/>
-                            <button id={el.id} onClick={handleLike}  className={ s.like}>
+                            <button id={el.id} onClick={handleLike} className={s.like}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"
-                                     focusable="false" className={!isLike ? s.active:s.favoriteIcon}>
+                                     focusable="false" className={likedId.includes(el.id) ? s.active : s.favoriteIcon}>
                                     <path
                                         d="M12 21.35 10.55 20.03C5.4 15.36 2 12.27 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41 0.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.77-3.4 6.86-8.55 11.54L12 21.35z"></path>
                                 </svg>
