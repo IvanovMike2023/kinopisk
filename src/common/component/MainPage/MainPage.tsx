1994c3988e1e277b43be46ea1065e9643a1c17a9
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2026-03-20 11:59:51 +0300

    error

--- src/common/component/MainPage/MainPage.tsx
+++ src/common/component/MainPage/MainPage.tsx
@@ -20,44 +20,8 @@ export const MainPage = () => {
     const now_playing_movies = NowPlayingData?.results ? NowPlayingData?.results.slice(0, 6): []
     const popular_movies =Popular?.results ? Popular?.results.slice(0, 6) : []
 
-
-        const [startTime, setStartTime] = useState(null);
-        const [now, setNow] = useState(null);
-        // 1. Создаем ref для хранения ID интервала
-        // Это значение не будет сбрасываться при рендерах
-        const timerRef = useRef(null);
-
-        function handleStart() {
-            setStartTime(Date.now());
-            setNow(Date.now());
-
-            clearInterval(timerRef.current);
-
-            // 2. Записываем ID в .current
-            timerRef.current = setInterval(() => {
-                setNow(Date.now());
-            }, 10);
-            console.log(timerRef)
-        }
-
-        function handleStop() {
-            // 3. Читаем ID из .current, чтобы остановить таймер
-            clearInterval(timerRef.current);
-        }
-
-        let secondsPassed = 0;
-        if (startTime != null && now != null) {
-            secondsPassed = (now - startTime) / 1000;
-        }
-
-
-
-
-
     return <div className={s.Container}>
-        <h1>Прошло времени: {secondsPassed.toFixed(3)}</h1>
-        <button onClick={handleStart}>Старт</button>
-        <button onClick={handleStop}>Стоп</button>
+
         <section className={s.page}>
             <section style={{backgroundImage: `url(https://image.tmdb.org/t/p/original/${backdrop_path})`}}
                      className={s.section}>
--- src/common/helper/useThemeMode.ts
+++ src/common/helper/useThemeMode.ts
@@ -3,15 +3,12 @@ import {useEffect, useState} from "react";
 export const useThemeMode=()=>{
     const [darkMode, setDarkMode] = useState(()=>{
         const isdarkMode= localStorage.getItem('theme')
-        //console.log(JSON.stringify(isdarkMode))
-        console.log(isdarkMode)
         return isdarkMode ? isdarkMode:true
     });
     const toggleTheme = () => {
         setDarkMode((prev)=>!prev);
     };
     useEffect(()=>{
-        //localStorage.getItem('theme')
         localStorage.setItem('theme',darkMode)
     },[darkMode])
 return [darkMode,toggleTheme]
