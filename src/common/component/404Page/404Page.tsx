7a2f60d0a2ad02a1cbc9645ab1c54f5bb17531ea
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2026-03-25 15:15:37 +0300

    NotFoundPage !

--- src/common/component/404Page/404Page.tsx
+++ src/common/component/404Page/404Page.tsx
@@ -5,17 +5,13 @@ export const NotFoundPage = () => {
     return (
         <div className={s.container}>
             <div className={s.content}>
-
                 <h1 className={s.code}>404</h1>
-
                 <h2 className={s.title}>
                     Page Not Found
                 </h2>
-
                 <p className={s.text}>
                     Sorry, the page you are looking for does not exist.
                 </p>
-
                 <Link to="/" className={s.button}>
                     Go Home
                 </Link>
