61117d5168b72f3686dc4ddd6c9a5ff91fe429b6
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2026-04-01 17:58:50 +0300

    logo

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,4 +1,4 @@
-import logoUrl from '../../img/logo.svg'
+import logoUrl from '../../img/logo.svg?url'
 import {AppBar, Container,  LinearProgress,  Toolbar, useTheme} from "@mui/material";
 import type { PaletteMode} from "@mui/material";
 import {NavLink, useNavigate} from "react-router-dom";
